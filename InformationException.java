c3fd9d8d01652ed483f3087c44e04e4757a7bda2
namht <namht@vt.edu>
2020-11-21 18:22:10 -0500

    InformationException

---
+++ InformationException.java
@@ -0,0 +1,13 @@
+/**
+* InformationException class.
+*
+* @author Nam Tran
+* @date 11.20.20
+ */
+import java.util.Exception;
+
+public class InformationException extends Exception {
+    public InformationException(String message) {
+        super(message);
+    }
+}
