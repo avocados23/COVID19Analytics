f5cde238b63c44dcee7a79f1627a17d2d162724b
jonathana <jonathana@vt.edu>
2020-11-21 18:32:52 -0500

    edit class

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those
+// who do.
+// --Jonathan Awad (jonathana)
+
+/**
+ * 
+ * @author Jonathan Awad (jonathana)
+ *
+ * @param <T>
+ *            the type of object that the list holds
+ */
+public class LinkedList<T> {
+
+    private int size;
+    private Node<T> head;
+}
