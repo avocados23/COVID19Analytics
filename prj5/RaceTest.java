140d8edba9af29bb98060822a95c57be3df7e242
jonathana <jonathana@vt.edu>
2020-11-21 20:04:38 -0500

    RaceTest.java

---
+++ prj5/RaceTest.java
@@ -0,0 +1,93 @@
+package prj5;
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those
+// who do.
+// --Jonathan Awad (jonathana)
+
+/**
+ * Tests the Race class
+ * 
+ * @author Jonathan Awad (jonathana)
+ * @version 2020.11.21
+ */
+public class RaceTest extends student.TestCase {
+
+    private Race race;
+    
+    /**
+     * setUp() method
+     */
+    public void setUp() {
+        race = new Race("test", 1, 5);
+    }
+    
+    /**
+     * tests the getRaceName() method
+     */
+    public void testGetRaceName() {
+        assertEquals("test", race.getRaceName());
+    }
+    
+    /**
+     * tests the getRaceName() method
+     */
+    public void testGetCaseNum() {
+        assertEquals(1, race.getCaseNum());
+    }
+    
+    /**
+     * tests the getRaceName() method
+     */
+    public void testGetDeathNum() {
+        assertEquals(5, race.getDeathNum());
+    }
+    
+    /**
+     * test setCaseNum() method
+     */
+    public void testSetCaseNum() {
+        race.setCaseNum(3);
+        assertEquals(3, race.getCaseNum());
+    }
+    
+    /**
+     * test setDeathNum() method
+     */
+    public void testSetDeathNum() {
+        race.setDeathNum(10);
+        assertEquals(10, race.getDeathNum());
+    }
+    
+    /**
+     * test CFR() method
+     */
+    public void testCFR() {
+        race.setDeathNum(1);
+        race.setCaseNum(1);
+        assertEquals(100, race.CFR(), .1);
+    }
+    
+    /**
+     * test toString() method
+     */
+    public void testToString() {
+        assertEquals("test: 1 cases, 500.0% CFR", race.toString());
+    }
+    
+    /**
+     * test equals() method
+     */
+    public void testEquals() {
+        Race other = new Race("test", 1, 5);
+        assertTrue(race.equals(other));
+        Race test = new Race("wrong", 1, 5);
+        assertFalse(race.equals(test));
+        other.setCaseNum(10);
+        assertFalse(race.equals(other));
+        Race other1 = new Race("test", 1, 6);
+        assertFalse(race.equals(other1));
+        assertFalse(race.equals("t"));
+    }
+}
