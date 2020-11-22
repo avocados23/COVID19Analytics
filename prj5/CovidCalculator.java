7a0e399092596e3e378743e231710af3f15f1686
namht <namht@vt.edu>
2020-11-21 21:40:12 -0500

    covidreeader nam version

--- prj5/CovidCalculator.java
+++ prj5/CovidCalculator.java
@@ -1,5 +1,19 @@
 package prj5;
 
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Nam Tran (namht)
+
+/**
+ * CovidCalculator class
+ * 
+ * @author Nam Tran (namht), Jonathan Awad
+ * @version 11.20.20
+ */
+
 public class CovidCalculator {
 
 }
