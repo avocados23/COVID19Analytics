9fa93f7521990d6314948f95abb16287f5d7b3de
jonathana <jonathana@vt.edu>
2020-11-21 22:20:00 -0500

    CovidCalc

--- prj5/CovidCalculator.java
+++ prj5/CovidCalculator.java
@@ -1,5 +1,12 @@
 package prj5;
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those
+// who do.
+// --Jonathan Awad (jonathana)
 
+<<<<<<< Updated upstream
 // Virginia Tech Honor Code Pledge:
 //
 // As a Hokie, I will conduct myself with honor and integrity at all times.
@@ -14,6 +21,48 @@ package prj5;
  * @version 11.20.20
  */
 
+=======
+import java.util.Arrays;
+
+/**
+ * Sorts the states by alphabetical order and the races by highest to lowest CFR
+ * 
+ * @author Jonathan Awad (jonathana)
+ * @verson 2020.11.21
+ */
+>>>>>>> Stashed changes
 public class CovidCalculator {
 
+    /**
+     * the total number of states
+     */
+    public static final int NUM_STATES = 6;
+    private LinkedList<State> linkedList;
+    private State[] states;
+
+    /**
+     * CovidCalculator constructor
+     * 
+     * @param stateName
+     * @param list
+     */
+    public CovidCalculator(State[] stateName) {
+        states = new State[NUM_STATES];
+    }
+
+    /**
+     * sorts the states by alphabetical order
+     */
+    public void sortByABC() {
+        State[] sortAlpha = Arrays.copyOf(states, states.length);
+        Arrays.sort(sortAlpha, 1, sortAlpha.length);
+    }
+
+
+    /**
+     * sorts the states by Case Fatality Ratio
+     */
+    public void sortByCFR() {
+
+    }
 }
